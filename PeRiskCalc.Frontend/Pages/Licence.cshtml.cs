a056b5c41d57513334342ae6ca444b7274921377
Nicholas Russell <nicholas.russell@nottingham.ac.uk>
2024-08-23 14:59:48 +0100

    Adds draft of new calc form design

---
+++ PeRiskCalc.Frontend/Pages/Licence.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PeRiskCalc.Frontend.Pages
+{
+    public class LicenceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
