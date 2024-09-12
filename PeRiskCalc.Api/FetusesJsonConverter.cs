b77cf96779e802af4517909da6f19b06da6a7ac8
Nicholas Russell <nicholas.russell@nottingham.ac.uk>
2024-09-12 11:44:04 +0100

    Implements FetusJsonConverter

---
+++ PeRiskCalc.Api/FetusesJsonConverter.cs
@@ -0,0 +1,29 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class FetusesJsonConverter : JsonConverter<Fetuses>
+{
+    public override Fetuses Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var value = reader.GetString();
+        return value.ToLower() switch
+        {
+            "singleton" => Fetuses.Singleton,
+            "twins-mono" => Fetuses.TwinMonochorionic,
+            "twins-di" => Fetuses.TwinDicorionic,
+            _ => throw new JsonException("Invalid value for Fetuses")
+        };
+    }
+
+    public override void Write(Utf8JsonWriter writer, Fetuses value, JsonSerializerOptions options)
+    {
+        var stringValue = value switch
+        {
+            Fetuses.Singleton => "singleton",
+            Fetuses.TwinMonochorionic => "twins-mono",
+            Fetuses.TwinDicorionic => "twins-di",
+            _ => throw new JsonException("Invalid value for Fetuses")
+        };
+        writer.WriteStringValue(stringValue);
+    }
+}
